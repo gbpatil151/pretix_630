6bd268c2388c238adff92e12f6afd43180dd225a
adityaanilraut <adityaanilraut@gmail.com>
2026-02-27 22:30:17 -0800

    Fix CI: apply flake8, isort, and license headers

--- src/tests/base/test_services_orders.py
+++ src/tests/base/test_services_orders.py
@@ -1,12 +1,41 @@
-import pytest
+#
+# This file is part of pretix (Community Edition).
+#
+# Copyright (C) 2014-2020  Raphael Michel and contributors
+# Copyright (C) 2020-today pretix GmbH and contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation in version 3 of the License.
+#
+# ADDITIONAL TERMS APPLY: Pursuant to Section 7 of the GNU Affero General
+# Public License, additional terms are applicable granting you additional
+# permissions and placing additional restrictions on your usage of
+# this software. Please refer to the pretix LICENSE file to obtain the full
+# terms applicable to this work. If you did not receive this file, see
+# <https://pretix.eu/about/en/license>.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 from datetime import timedelta
 from decimal import Decimal
 
+import pytest
 from django.utils.timezone import now
 from django_scopes import scope
 
-from pretix.base.models import Event, Item, ItemVariation, Order, OrderPosition, Organizer
-from pretix.base.services.orders import approve_order, deny_order, OrderError
+from pretix.base.models import (
+    Event, Item, ItemVariation, Order, OrderPosition, Organizer,
+)
+from pretix.base.services.orders import OrderError, approve_order, deny_order
+
 
 @pytest.fixture
 def order_setup():
@@ -23,7 +52,9 @@ def order_setup():
         sales_channel=o.sales_channels.get(identifier="web"),
     )
     shirt = Item.objects.create(event=event, name='T-Shirt', default_price=12)
-    shirt_red = ItemVariation.objects.create(item=shirt, default_price=14, value="Red")
+    shirt_red = ItemVariation.objects.create(
+        item=shirt, default_price=14, value="Red"
+    )
     OrderPosition.objects.create(
         order=o1, item=shirt, variation=shirt_red,
         price=12, attendee_name_parts={}, secret='1234'
@@ -37,7 +68,9 @@ def test_approve_order_requires_approval(order_setup):
     with scope(organizer=event.organizer):
         order.require_approval = False
         order.save()
-        with pytest.raises(OrderError, match="This order is not pending approval."):
+        with pytest.raises(
+            OrderError, match="This order is not pending approval."
+        ):
             approve_order(order, send_mail=False)
 
 
@@ -58,7 +91,9 @@ def test_deny_order_requires_approval(order_setup):
     with scope(organizer=event.organizer):
         order.require_approval = False
         order.save()
-        with pytest.raises(OrderError, match="This order is not pending approval."):
+        with pytest.raises(
+            OrderError, match="This order is not pending approval."
+        ):
             deny_order(order, send_mail=False)
 
 
